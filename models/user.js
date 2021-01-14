18155a2fa9ed8b1ac27ada80d29aab93b2527f74
Jermaine <jermzblake@gmail.com>
2021-01-14 18:46:48 -0500

    code User model

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: {type: String, required: true, lowercase: true, unique: true},
+    password: String,
+    googleID: String,
+    avatar: String
+  }, {
+    timestamps: true
+  });
+
+module.exports = mongoose.model('User', userSchema);
