0418b48fea666a96874313869c5c249f07b23924
Jermaine <jermzblake@gmail.com>
2021-01-20 17:19:06 -0500

    create helper module in config. code auth checker in helper module

---
+++ config/helpers.js
@@ -0,0 +1,7 @@
+/*----- Helper Functions -----*/
+function checkAuth(req, res, next) {
+    if (req.user) return next();
+    return res.status(401).json({msg: 'Not Authorized'});
+} 
+
+module.exports = {checkAuth}
