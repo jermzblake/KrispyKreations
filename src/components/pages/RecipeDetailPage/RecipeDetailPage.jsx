26560141f992d6422cc385c0abee106fa10e5af2
Jermaine <jermzblake@gmail.com>
2021-06-03 08:50:28 -0400

    add dialog and make header a link

--- src/components/pages/RecipeDetailPage/RecipeDetailPage.jsx
+++ src/components/pages/RecipeDetailPage/RecipeDetailPage.jsx
@@ -7,6 +7,16 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid'
 import { makeStyles, Box } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
 
 const useStyles = makeStyles((theme) =>({
     root: {
@@ -24,6 +34,10 @@ const useStyles = makeStyles((theme) =>({
         display: 'block',
         marginLeft: 'auto',
         marginRight: 'auto',
+    },
+    headerLink: {
+        textDecoration: 'none',
+        color: '#6B705C'
     }
 }))
 
@@ -31,6 +45,7 @@ export default function RecipeDetailPage({match, history, user}) {
     const classes = useStyles()
     const [recipe, setRecipe] = useState('')
     const [ingredients, setIngredients] = useState([])
+    const [open, setOpen] = useState(false);
     
     useEffect(() => {
         if(!recipe) return loadRecipe(match.params.id);
@@ -47,16 +62,25 @@ export default function RecipeDetailPage({match, history, user}) {
     }
 
     const handleDelete = async (e) => {
+        setOpen(false);
         await recipeService.deleteEntry(recipe)
         history.push('/recipebook')
     }
+
+    const handleClickOpen = () => {
+        setOpen(true);
+      };
+    
+      const handleClose = () => {
+        setOpen(false);
+      };
 
     // if user's recipe has an image display it otherwise line break
     const imageDisplay = (recipe.image) ? <img src={recipe.image}  alt="food"/> : <br />
 
     return (
         <>
-            <h1>{user.name}'s Recipe Book</h1>
+            <Link to='/recipebook' className={classes.headerLink}><h1>{user.name}'s Recipe Book</h1></Link>
             <Link to='/recipeform'>Create New Recipe</Link>
 
             <div className='page-container'>
@@ -96,11 +120,34 @@ export default function RecipeDetailPage({match, history, user}) {
                     className={classes.mRL} 
                     variant="contained" 
                     color="secondary" 
-                    onClick={handleDelete}
+                    onClick={handleClickOpen}
                     startIcon={<DeleteIcon />}
                 >
                     DELETE
                 </Button>
+                <Dialog
+                    open={open}
+                    TransitionComponent={Transition}
+                    keepMounted
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-slide-title"
+                    aria-describedby="alert-dialog-slide-description"
+                >
+                    <DialogTitle id="alert-dialog-slide-title">{"Are you sure you want to delete this recipe?"}</DialogTitle>
+                    <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        Deleting a recipe cannot be undone. Please confirm you want to delete. If you got here by mistake just hit cancel.
+                    </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleDelete} color="primary">
+                        Confirm Delete
+                    </Button>
+                    </DialogActions>
+                </Dialog>
                 </Grid>
                 </Grid>
             </div>
