cbbea7fc3f25f87a145419ad79cecf24c69f1374
Jermaine <jermzblake@gmail.com>
2021-06-04 01:23:54 -0400

    change recipe name colour

--- src/components/RecipeBookEntries/RecipeBookEntries.jsx
+++ src/components/RecipeBookEntries/RecipeBookEntries.jsx
@@ -6,10 +6,10 @@ function RecipeBookEntries(props) {
 
     return (
       <>
-        <Link to={`/recipebook/${props.recipeEntry._id}`} >
+        <Link className={"link-container"} to={`/recipebook/${props.recipeEntry._id}`} >
           <div className="entry-container">
             <div>
-              <h2>{props.recipeEntry.name}</h2>
+              <h2 className="recipe">{props.recipeEntry.name}</h2>
               <span className='category'>{props.recipeEntry.category}</span>
             </div>
             <img src={props.recipeEntry.image || "https://cdn1.vectorstock.com/i/thumb-large/49/10/cartoon-dachshund-chef-with-a-spoon-vector-19784910.jpg"}  alt="food"/>
